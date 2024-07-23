1d8b34114392f610dc620552ba5c1cc3a1aa46eb
czxdev <36617515+czxdev@users.noreply.github.com>
2024-07-23 09:46:41 +0800

    Add data augmentation to data preprocessing

--- tasks/models/NNClassifier.py
+++ tasks/models/NNClassifier.py
@@ -26,11 +26,16 @@ class NNClassifier():
     MIX_PRECISION = True
     
     @staticmethod
-    def preprocessing(x:np.ndarray, y:np.ndarray = None, *, input_channels = 3, image_shape = (32,32), mean = None, std = None, **kwargs) -> tuple[torch.Tensor, torch.Tensor]:
+    def preprocessing(x:np.ndarray, y:np.ndarray = None, *, input_channels = 3, image_shape = (32,32),
+                      mean = None, std = None, train = False, **kwargs) -> tuple[torch.Tensor, torch.Tensor]:
         '''The default values here is for Cifar10 dataset, which has 3 channels and 32x32 images'''
         images = []
-        IMAGE_TRANSFORM = transforms.Compose([transforms.ToTensor(), 
-                                          transforms.Normalize(mean or [0.5]*input_channels, std or [0.5]*input_channels)])
+        transform_sequence = [transforms.ToTensor(), 
+                              transforms.Normalize(mean or [0.5]*input_channels, std or [0.5]*input_channels)]
+        if train:
+            transform_sequence = [transforms.RandomCrop(32, padding=4),
+                                  transforms.RandomHorizontalFlip()] + transform_sequence
+        IMAGE_TRANSFORM = transforms.Compose(transform_sequence)
         width, height = image_shape
         if input_channels == 1:
             x_reshape = x.reshape(-1, width, height)
@@ -111,7 +116,7 @@ class NNClassifier():
             self.classes_, y_encoded = np.unique(y, return_inverse=True)
         else:
             y_encoded = np.searchsorted(self.classes_, y)
-        x_tensor_train, y_tensor_train = self.preprocessing(x, y_encoded, **self.nn_params)
+        x_tensor_train, y_tensor_train = self.preprocessing(x, y_encoded, train=True, **self.nn_params)
         training_dataset = torch.utils.data.TensorDataset(x_tensor_train, y_tensor_train)
         training_loader = torch.utils.data.DataLoader(training_dataset, 
                                                       batch_size=NNClassifier.TRAINING_BATCH_SIZE,
